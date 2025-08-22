c5d9417684507a248ca2a2abd6a1faaa5f8b3d73
daanniill <daniil.malakhov12@gmail.com>
2025-08-22 13:52:28 -0700

    redid the createBoard function which makes the tictactoe board so it fits the logic design better

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
-function create_board() {
-    const board = document.querySelector(".board")
-    for (let i = 0; i < 9; i++) {
-        let box = document.createElement("div")
-        box.setAttribute("class", "box")
-        board.appendChild(box)
-    }
-}
+const Game = (function() {
+    let board = [...Array(3).map(e => Array(3).fill(''))]
 
-create_board()
+    const createBoard = () => {
+        const board = document.querySelector(".board")
+        for (let i = 0; i < 3; i++) {
+            let row = document.createElement("div")
+            row.setAttribute("class", "row")
+            for (let j = 0; j < 3; j++)
+            {
+                let box = document.createElement("div")
+                box.setAttribute("class", "box")
+                row.appendChild(box)
+            }
+            board.appendChild(row)
+        }
+    };
+
+    const displayControl = () => {
+        
+    };
+
+    return {board, createBoard}
+
+})();
+
+Game.createBoard()
